6cb005a36ca4de45c129865b3dab9e71b051b707
Dmitrii <37974778+DmitryPMI@users.noreply.github.com>
2018-10-21 22:22:31 +0300

    Add files via upload

--- bot/blog/views.py
+++ bot/blog/views.py
@@ -16,17 +16,30 @@ def events_add_query(request):
 	re.save()
 	return HttpResponseRedirect(reverse('events_page'))
 
+
 def events_page(request):
 	return render(request, 'blog/events_add.html')
+
+
+def events_delete(request):
+	event = Event.objects.get(text=request.POST["text_event"], post_date=request.POST["date_event"])
+	event.delete()
+	return HttpResponseRedirect(reverse('event_list_url'))
+
+def events_delete_page(request):
+	return render(request, 'blog/event_list_url.html')
+
 
 def intro_question_list(request):
 	questions = IntroQuestion.objects.all()
 	return render(request, 'blog/question_list.html', context={'questions': questions})
 
+
 def intro_add_query(request):
 	re = IntroQuestion(text=request.POST["text_question"])
 	re.save()
 	return HttpResponseRedirect(reverse('intro_page'))
+
 
 def intro_page(request):
 	return render(request, 'blog/intro_add.html')
